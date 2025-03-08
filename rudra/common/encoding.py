6d95de2ed121403e96c06b1152c02b8cf4b7e957
bruhat15 <kulkarnibruhat@gmail.com>
2025-03-08 15:01:30 +0530

    Centralized Common Functions in a new folder -common-

--- rudra/common/encoding.py
+++ rudra/common/encoding.py
@@ -1,20 +1,4 @@
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, StandardScaler,LabelEncoder
-
-def min_max_scale(df: pd.DataFrame) -> pd.DataFrame:
-    """Applies Min-Max Scaling (0 to 1) to numerical columns."""
-    scaler = MinMaxScaler()
-    numerical_cols = df.select_dtypes(include=['number']).columns
-    df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
-    return df
-
-def normalize(df: pd.DataFrame) -> pd.DataFrame:
-    """Normalizes numerical columns (Z-score standardization)."""
-    scaler = StandardScaler()
-    numerical_cols = df.select_dtypes(include=['number']).columns
-    df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
-    return df
-
+from sklearn.preprocessing import LabelEncoder
 def encode_features(df: pd.DataFrame) -> pd.DataFrame:
     """ Encodes categorical columns using One-Hot Encoding for multi-class variables  
     and Label Encoding for binary categories."""
